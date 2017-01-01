5186b0022432b4ecd5c25bc28424710b729f7de2
Ahmed T. Youssef <xmonader@gmail.com>
2017-01-01 13:58:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require("http")
+
+http.createServer(function(request, response){
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write("Hello, World!");
+	response.end();
+
+
+}).listen(3002);
+console.log('Server started...')
